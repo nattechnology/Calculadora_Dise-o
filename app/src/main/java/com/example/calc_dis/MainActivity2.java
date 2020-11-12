8bfb95192190f2636d6db4c9700f04b6e0439f08
URIEL ANGEL <nattechnology02@gmail.com>
2020-11-11 21:06:48 -0600

    version 2

---
+++ app/src/main/java/com/example/calc_dis/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.calc_dis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivityForResult(intent, 0);
+                finish();
+            }
+        }, 10000);
+    }
+}
